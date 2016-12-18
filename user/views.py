2a949e573ed2305bf8ba478a71a7d576c160cefb
jossyijezie <jossyijezie>
2016-12-17 19:11:40 -0800

    initial commit

---
+++ user/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+# Create your views here.
+def index(request):
+	# return HttpResponse("Hello, World. You're at the polls index")
